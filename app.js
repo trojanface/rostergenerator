31cbe7baf51234219b0732c8eb2405ea56c9eeb5
MattT <trojanface@hotmail.com>
2020-05-01 10:43:48 +0930

    Changed questions to objects

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Question = require("./lib/Question");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,6 +10,8 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+let employees = [];
 //asks what type of team member you're adding
 //asks common questions
 //asks specialised questions depending on type of member
@@ -16,36 +19,89 @@ const render = require("./lib/htmlRenderer");
 //in main script the created objects are stored in an array, once finished it loops over the array and generates html from it.
 //asks if you want to add another member if not then it tells main script to generate otherwise it calls its question method again.
 console.log("\nWelcome to team roster generator!\n")
-questionTime();
-async function questionTime() {
+addEmployee();
+async function addEmployee() {
     try {
-        const questData = await inquirer.prompt([
-            {
-                type: "input",
-                name: "employeeType",
-                message: "What type of employee would you like to add? (Manager, Engineer, Intern)",
-                //is validated as the user MUST supply their username.
-                validate: (input) => {
-                    if (input === "") {
-                        return "Must be answered";
-                    } else {
-                        if (input.toLowerCase() !== "manager" && input.toLowerCase() !== "engineer" && input.toLowerCase() !== "intern") {
-                            return "Must be either Manager, Engineer or Intern"
-                        } else {
-                            return true;
-                        }
-                    }
-
-                }
-            }
+        let questData;
+        if (employees.length === 0) {
+        questData = await inquirer.prompt([
+            new Question("confirm","addAnother","Would you like to add an employee?")
         ]);
-        console.log(questData);
+    } else {
+        questData = await inquirer.prompt([
+            new Question("confirm","addAnother","Would you like to add another employee?")
+        ]);
+    }
+        if (questData.addAnother === true) {
+            employeeType();
+        } else {
+            console.log("\nExiting now"); //this will instead redirect to the gernate html function
+                process.exit();
+        }
     } catch (err) {
         throw err;
     }
 }
 
+async function employeeType() {
+    try {
+        const questData = await inquirer.prompt([
+            
+                new Question("input","employeeType","What type of employee would you like to add? (Manager, Engineer, Intern)")
+                
+        ]);
+        for (property in questData) {//capitalises position title.        
+            questData[property] = `${questData[property][0].toUpperCase()}${questData[property].substring(1)}`;
+        }
+        mainQuestions(questData[property]);
+    } catch (err) {
+        throw err;
+    }
+}
 
+async function mainQuestions(role) {
+    try {
+        const questData = await inquirer.prompt([
+            new Question("input","employeeName","Employee name:"),
+            new Question("input","employeeID","Employee id:"),
+            new Question("input","employeeEmail","Employee email address:")
+        ]);
+        specialisedQuestions(role, questData);
+    } catch (err) {
+        throw err;
+    }
+}
+
+async function specialisedQuestions(role, data) {
+    console.log(data);
+    let questData;
+    try {
+        switch (role) {
+
+            case ("Manager"):
+                questData = await inquirer.prompt([
+                    new Question("input","specificInfo","Office number:")
+                ]);
+                break;
+            case ("Engineer"):
+                questData = await inquirer.prompt([
+                    new Question("input","specificInfo","Github username:")
+                ]);
+                break;
+            case ("Intern"):
+                questData = await inquirer.prompt([
+                    new Question("input","specificInfo","School:")
+                ]);
+                break;
+            default:
+        }
+
+        employees.push(generateMember(data.employeeName, data.employeeID, data.employeeEmail, role, questData.specificInfo));
+        addEmployee();
+    } catch (err) {
+        throw err;
+    }
+}
 
 function generateMember(name, id, email, role, specificV) {
     switch (role) {
@@ -68,7 +124,6 @@ function generateMember(name, id, email, role, specificV) {
 
 }
 
-module.exports = generateMember();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
