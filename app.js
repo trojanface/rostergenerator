43610b7ae9fb0738764537dec4047999cee20255
MattT <trojanface@hotmail.com>
2020-05-03 14:05:33 +0930

    Added final styling and useful commenting for future developers.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Importing required objects/dependencies/scripts
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -5,76 +6,68 @@ const Question = require("./lib/Question");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
+const render = require("./lib/htmlRenderer");
+//creating output path
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./lib/htmlRenderer");
-
+//creates an empty employees array to add new employees to.
 let employees = [];
-//asks what type of team member you're adding
-//asks common questions
-//asks specialised questions depending on type of member
-//returns a created object --can test that the object is created properly
-//in main script the created objects are stored in an array, once finished it loops over the array and generates html from it.
-//asks if you want to add another member if not then it tells main script to generate otherwise it calls its question method again.
+//The welcome message that begins the series of questions.
 console.log("\nWelcome to team roster generator!\n")
 addEmployee();
 async function addEmployee() {
     try {
         let questData;
-        if (employees.length === 0) {
-            questData = await inquirer.prompt([
-                new Question("confirm", "addAnother", "Would you like to add an employee?")
-            ]);
+        if (employees.length === 0) {//If there are no entries yet then it asks about the manager first
+           console.log("Please answer the following questions about the manager of the team.\n")
+           mainQuestions("Manager");//launches the questions function with the manager type.
         } else {
-            questData = await inquirer.prompt([
+            questData = await inquirer.prompt([//If there are entries then it prompts if you'd like to add more
                 new Question("confirm", "addAnother", "Would you like to add another employee?")
             ]);
-        }
-        if (questData.addAnother === true) {
-            employeeType();
-        } else {
-            
-            if (employees.length > 0) {
-                console.log("\nGenerating your roster HTML");
-            fs.writeFile("./output/team.html", render(employees), (err) => {
-                if (err) {
-                    throw err;
+            if (questData.addAnother === true) {
+                employeeType();//Launches the type question to clarify what type of employee you're adding.
+            } else {
+    
+                if (employees.length > 0) { //If you don't want to add more employees then it will generate the HTML template
+                    console.log("\nGenerating your roster HTML");
+                    fs.writeFile(outputPath, render(employees), (err) => {
+                        if (err) {
+                            throw err;
+                        }
+                    });
+                    fs.writeFile(path.join(OUTPUT_DIR, "style.css"),fs.readFileSync("./style.css"), (err) => {
+                        if (err) {
+                            throw err;
+                        }
+                        process.exit();
+                    });
+                } else {
+                    console.log("\nExiting without generating a roster");//If the array is empty then quite the script without trying to generate the HTML.
+                    process.exit();
                 }
-                process.exit();
-            });
-        } else {
-            console.log("\nExiting without generating a roster");
-            process.exit();
+            }
         }
-
-
-
-
-        }
+        
     } catch (err) {
         throw err;
     }
 }
 
-async function employeeType() {
+async function employeeType() {//Clarifies which type of employee you'd like to enter.
     try {
         const questData = await inquirer.prompt([
-
-            new Question("input", "employeeType", "What type of employee would you like to add? (Manager, Engineer, Intern)")
+            new Question("input", "employeeTypeQ", "What type of employee would you like to add? (Engineer, Intern)")
 
         ]);
-        for (property in questData) {//capitalises position title.        
-            questData[property] = `${questData[property][0].toUpperCase()}${questData[property].substring(1)}`;
-        }
-        mainQuestions(questData[property]);
+        questData.employeeTypeQ = `${questData.employeeTypeQ[0].toUpperCase()}${questData.employeeTypeQ.substring(1)}`;
+        mainQuestions(questData.employeeTypeQ);
     } catch (err) {
         throw err;
     }
 }
 
-async function mainQuestions(role) {
+async function mainQuestions(role) {//The few questions that are common no matter the type of employee
     try {
         const questData = await inquirer.prompt([
             new Question("input", "employeeName", "Employee name:"),
@@ -87,8 +80,7 @@ async function mainQuestions(role) {
     }
 }
 
-async function specialisedQuestions(role, data) {
-    console.log(data);
+async function specialisedQuestions(role, data) {//A function that asks questions depending upon the type of employee selected.
     let questData;
     try {
         switch (role) {
@@ -97,68 +89,25 @@ async function specialisedQuestions(role, data) {
                 questData = await inquirer.prompt([
                     new Question("input", "specificInfo", "Office number:")
                 ]);
+                employees.push(new Manager(data.employeeName, data.employeeID, data.employeeEmail, questData.specificInfo));
                 break;
             case ("Engineer"):
                 questData = await inquirer.prompt([
                     new Question("input", "specificInfo", "Github username:")
                 ]);
+                employees.push(new Engineer(data.employeeName, data.employeeID, data.employeeEmail, questData.specificInfo));
                 break;
             case ("Intern"):
                 questData = await inquirer.prompt([
                     new Question("input", "specificInfo", "School:")
                 ]);
+                employees.push(new Intern(data.employeeName, data.employeeID, data.employeeEmail, questData.specificInfo));
                 break;
             default:
         }
-
-        employees.push(generateMember(data.employeeName, data.employeeID, data.employeeEmail, role, questData.specificInfo));
         addEmployee();
     } catch (err) {
         throw err;
     }
 }
 
-function generateMember(name, id, email, role, specificV) {
-    switch (role) {
-        case "Employee":
-            return new Employee(name, id, email)
-            break;
-        case "Engineer":
-            return new Engineer(name, id, email, specificV)
-            break;
-        case "Intern":
-            return new Intern(name, id, email, specificV)
-            break;
-        case "Manager":
-            return new Manager(name, id, email, specificV)
-            break;
-        default:
-
-            break;
-    }
-
-}
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
